fb2489c8356fee82e6bd91ac6606f09aebd016b2
Lauren Marcus <lauren.e.marcus@gmail.com>
2015-03-20 11:05:35 -0700

    Save comments to the database.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,20 @@ class CommentsController <ApplicationController
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new
   end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    if @comment.save
+      flash[:notice] = "Comment successfully added."
+      redirect_to post_path(@comment.post)
+    else
+      render :new
+    end
+  end
+
+private
+  def comment_params
+    params.require(:comment).permit(:reply)
+  end
 end
