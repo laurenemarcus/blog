0403206c0ba1203ebf600be9935ab6245a6a3e85
Lauren Marcus <lauren.e.marcus@gmail.com>
2015-03-20 09:37:04 -0700

    Validate the presence of the blog entry.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, :presence => true
   validates :date, :presence => true
+  validates :entry, :presence => true
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 describe Post do
   it { should validate_presence_of :title }
   it { should validate_presence_of :date }
+  it { should validate_presence_of :entry }
 end
