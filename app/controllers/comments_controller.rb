6e61335142cfaec9a5cd4ea7a08e78b6fd668dad
Lauren Marcus <lauren.e.marcus@gmail.com>
2015-04-03 11:51:52 -0700

    Render the partial showing all comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController <ApplicationController
   def new
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new
+    @comments = Comment.all
   end
 
   def create
